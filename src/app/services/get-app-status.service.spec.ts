625212eaaca1d793c6de48c45807d576a64053c4
Andrej Rasevic <andrej@rasevicengineering.com>
2019-05-16 23:09:24 -0400

    main service name refactor

---
+++ src/app/services/get-app-status.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GetAppStatusService } from './get-app-status.service';
+
+describe('GetAppStatusService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: GetAppStatusService = TestBed.get(GetAppStatusService);
+    expect(service).toBeTruthy();
+  });
+});
