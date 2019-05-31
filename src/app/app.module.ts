1c894b66eaf35166c1d0220e928e5f0d1bf02710
brendoncaulkins <brendon.caulkins@excella.com>
2019-05-31 14:20:57 -0400

    4: Move Material Import management to MaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-import { MatDividerModule, MatExpansionModule } from '@angular/material'
-
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
+import { MaterialModule } from './mat.module';
 import { NgModule } from '@angular/core'
 import { ServiceCardComponent } from './components/service-card/service-card.component'
 import { StatusComponent } from './components/status/status.component'
@@ -11,9 +9,7 @@ import { StatusComponent } from './components/status/status.component'
   declarations: [AppComponent, ServiceCardComponent, StatusComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatExpansionModule,
-    MatDividerModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/service-card/service-card.component.spec.ts
+++ src/app/components/service-card/service-card.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { IProcess, Status } from '../../models/page';
 
+import { MaterialModule } from '../../mat.module';
 import { ServiceCardComponent } from './service-card.component';
+import { StatusComponent } from '../status/status.component';
 
-describe('SiteComponent', () => {
+describe('ServiceCardComponent', () => {
   let component: ServiceCardComponent;
   let fixture: ComponentFixture<ServiceCardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServiceCardComponent ]
+      declarations: [ ServiceCardComponent, StatusComponent ],
+      imports: [ MaterialModule ]
     })
     .compileComponents();
   }));
@@ -16,6 +20,7 @@ describe('SiteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ServiceCardComponent);
     component = fixture.componentInstance;
+    component.process = {name: 'test service', status: Status.up} as IProcess
     fixture.detectChanges();
   });
 
