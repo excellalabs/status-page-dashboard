4f913b4c992cd461bea9f0eba11453e2f34f8734
Cdavies <cdavies@fandm.edu>
2019-05-31 17:04:43 -0400

    begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  template: '<app-dashboard></app-dashboard>',
+  template: `
+  <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+
+  <h1>Excella</h1>
+  <app-dashboard class="body"></app-dashboard>
+  `,
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {}
--- src/app/components/service-card/service-card.component.ts
+++ src/app/components/service-card/service-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core'
 @Component({
   selector: 'app-service-card',
   templateUrl: './service-card.component.html',
-  styleUrls: ['./service-card.component.css']
+  styleUrls: ['./service-card.component.scss']
 })
 export class ServiceCardComponent implements OnInit {
   @Input() process
