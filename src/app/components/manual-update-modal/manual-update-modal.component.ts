b926604df05167bf17191e188e69b9c9be720f28
Cdavies <cdavies@fandm.edu>
2019-06-13 17:23:27 -0400

    add update method to edit modal

--- src/app/components/manual-update-modal/manual-update-modal.component.ts
+++ src/app/components/manual-update-modal/manual-update-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core'
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material'
 import { GetAppStatusService } from '../../services/get-app-status.service'
-import { ServiceStatus, IService } from '../../models/service.interface'
+import { ServiceStatus, IService, UpdateMethod } from '../../models/service.interface'
 @Component({
   selector: 'app-manual-update-modal',
   templateUrl: './manual-update-modal.component.html',
@@ -10,6 +10,7 @@ import { ServiceStatus, IService } from '../../models/service.interface'
 export class ManualUpdateModalComponent {
   statuses: Array<string>
   service: IService
+  updateMethods: Array<string>
   constructor(
     public dialogRef: MatDialogRef<ManualUpdateModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
@@ -17,8 +18,9 @@ export class ManualUpdateModalComponent {
   ) {
     this.service = Object.assign({}, data.service)
     this.statuses = [ServiceStatus.UP, ServiceStatus.ISSUE, ServiceStatus.DOWN]
+    this.updateMethods = [UpdateMethod.manual, UpdateMethod.automatic]
   }
-
+  
   cancel(): void {
     this.dialogRef.close()
   }
