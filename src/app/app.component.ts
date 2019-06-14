332a23b2174d59a999944c36a87ffee462a17582
Samuel Rodriguez <surfersamu@samu.local>
2019-06-14 11:44:29 -0400

    added version to footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { ThemeService } from './services/theme.service';
 import { Observable } from 'rxjs';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,16 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
 
   isLightTheme$: Observable<boolean>;
+  AppVersion: string = version;
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit() {
     this.isLightTheme$ = this.themeService.isLightTheme$;
+  }
+
+  getVersion() {
+    return this.AppVersion;
   }
 
   toggleLightTheme(checked: boolean) {
